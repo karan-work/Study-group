c8b53b443bb39033b364ac8bf1e6603c156423db
Karan Satia <ksatia@BK-Karan-Satia.local>
2022-11-03 15:27:37 -0400

    sets default viewport size in config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    viewportHeight: 1000,
-  });
+  viewportHeight: 1000,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
