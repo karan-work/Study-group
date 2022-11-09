946f9f44649c522f3c910c7c8d9aed4392acd908
Karan Satia <ksatia@BK-Karan-Satia.local>
2022-11-09 16:49:12 -0500

    disables chromeWebSecurity to avoid CORS error

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   viewportHeight: 1000,
-
+  chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
